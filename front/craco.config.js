dd1b15118dc313f479bec1d8b164b60d5a2c2f32
FifthLord <nborysenko777@gmail.com>
2023-03-26 21:45:10 +0300

    try added alias, but no

---
+++ front/craco.config.js
@@ -0,0 +1,13 @@
+
+const { CracoAliasPlugin } = require('react-app-alias')
+
+const options = {} // default is empty for most cases
+
+module.exports = {
+   plugins: [
+      {
+         plugin: CracoAliasPlugin,
+         options: {}
+      }
+   ]
+}

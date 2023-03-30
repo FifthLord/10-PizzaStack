69dcedda9ba1f81eaa08229f05e6f7a9db289b70
FifthLord <nborysenko777@gmail.com>
2023-03-30 14:32:39 +0300

    start work with menu item

--- front/src/modules/menu/menu-item/menu-item.component.tsx
+++ front/src/modules/menu/menu-item/menu-item.component.tsx
@@ -1,3 +1,12 @@
 export const MenuItem = () => {
-   return <h1>menu item</h1>
+   return (
+      <div className="w-96 shadow-xl rounded-2xl">
+         <div className="">
+            <img className="object-cover object-center w-full h-[15rem] rounded-t-2xl"
+               src="/assets/pizza/manhattan.jpeg"
+            />
+            <span>555 г</span>
+         </div>
+      </div>
+   )
 }

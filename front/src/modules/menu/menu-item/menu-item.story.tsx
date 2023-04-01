5fb23beaba83fd3d1f49761ef3a595b46796ca21
FifthLord <nborysenko777@gmail.com>
2023-04-01 14:01:42 +0300

    work with interface

--- front/src/modules/menu/menu-item/menu-item.story.tsx
+++ front/src/modules/menu/menu-item/menu-item.story.tsx
@@ -7,6 +7,14 @@ export default {
    component: MenuItem,
 } as ComponentMeta<typeof MenuItem>;
 
-const Template: ComponentStory<typeof MenuItem> = () => <MenuItem />;
+const Template: ComponentStory<typeof MenuItem> = (args) => <MenuItem {...args} />;
 
 export const View = Template.bind({});
+
+View.args = {
+   imagePath: '/assets/pizza/manhattan.jpeg',
+   weight: 555,
+   title: 'Піца Мангеттен',
+   ingredients: '(подвійна порція грибів), Гриби, Моцарела, Пепероні, Соус Альфредо',
+   price: 215,
+}

7e412f62c416fef0b7b3f5a9b2a5a4399d444bf7
FifthLord <nborysenko777@gmail.com>
2023-04-08 11:58:02 +0300

    work with footer

--- front/src/common/components/footer/footer.component.tsx
+++ front/src/common/components/footer/footer.component.tsx
@@ -1,3 +1,23 @@
 export const Footer = () => {
-   return <h1>Footer</h1>
+   return <div className="bg-gray-900 pt-12 px-12 pb-8">
+      <div>
+         <div className="mb-4">
+            <span className="uppercase font-bold text-sm text-zinc-500">Контакти</span>
+         </div>
+         <div>
+            <ul>
+               <li>
+                  <a href="tel:+38044123456*"
+                     className="font-bold text-sm text-gray-400">
+                     044 123 45 67</a>
+               </li>
+               <li>
+                  <a href="email:info@pizzastack.app"
+                     className="font-bold text-sm text-gray-400">
+                     info@pizzastack.app</a>
+               </li>
+            </ul>
+         </div>
+      </div>
+   </div>
 }

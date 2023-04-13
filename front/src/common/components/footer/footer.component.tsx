01bfeb373bc282558d4410630b8a4b3fd35f31c5
FifthLord <nborysenko777@gmail.com>
2023-04-13 22:35:00 +0300

    finish footer

--- front/src/common/components/footer/footer.component.tsx
+++ front/src/common/components/footer/footer.component.tsx
@@ -6,13 +6,17 @@ export const Footer = () => {
          <div className="mb-4">
             <span className="uppercase font-bold text-sm text-zinc-500">Контакти</span>
          </div>
-         <div>
+         <div className="mb-8">
             <ul>
                <li>
                   <FooterLink href="tel:+38044123456*">044 123 45 67</FooterLink>
                   <FooterLink href="mailto:info@pizzastack.app">info@pizzastack.app</FooterLink>
                </li>
             </ul>
+         </div>
+         <hr className="relative left-[-3rem] w-[calc(100%_+_6rem)] border-zinc-500 mb-8" />
+         <div>
+            <span className="text-xl font-semibold text-white">PizzaStack</span>
          </div>
       </div>
    </div>

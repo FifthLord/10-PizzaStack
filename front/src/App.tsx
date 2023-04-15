8dc401214779064023c36beb49a34568014b3c92
FifthLord <nborysenko777@gmail.com>
2023-04-15 14:57:13 +0300

    combine ant settings components

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,15 @@
+import { Header } from "./common/components/header/header.component";
+import pizzaMenu from './mocks/pizza.json'
+import { MenuList } from "./modules/menu/components/menu-list/menu-list.component";
+
 export const App = () => {
    return (
-      <h1 className="font-bold text-3xl text-red-500">PizzaStack</h1>
+      <div>
+         <Header />
+         <div className="mx-12">
+            <MenuList items={pizzaMenu} />
+         </div>
+      </div>
+      // <h1 className="font-bold text-3xl text-red-500">PizzaStack</h1>
    );
 }
--- front/src/modules/menu/components/menu-list/menu-list.component.tsx
+++ front/src/modules/menu/components/menu-list/menu-list.component.tsx
@@ -7,7 +7,7 @@ interface MenuListProps {
 }
 
 export const MenuList: FC<MenuListProps> = ({ items }) => {
-   return <div className='flex flex-wrap gap-10'>
+   return <div className='flex flex-wrap gap-10 justify-center'>
       {items.map(({ image, ...pizza }) => (
          <MenuItem
             {...pizza} imagePath={'/assets/pizza/' + image}

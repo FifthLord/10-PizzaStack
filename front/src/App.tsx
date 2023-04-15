d1b4268de5c64cd863f8d704955ccc466127c158
FifthLord <nborysenko777@gmail.com>
2023-04-15 15:45:25 +0300

    create homepage

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./common/components/footer/footer.component";
 import { Header } from "./common/components/header/header.component";
 import pizzaMenu from './mocks/pizza.json'
 import { MenuList } from "./modules/menu/components/menu-list/menu-list.component";
@@ -6,9 +7,10 @@ export const App = () => {
    return (
       <div>
          <Header />
-         <div className="mx-12">
+         <div className="mx-12 mb-24">
             <MenuList items={pizzaMenu} />
          </div>
+         <Footer />
       </div>
       // <h1 className="font-bold text-3xl text-red-500">PizzaStack</h1>
    );
